d5a50f43bc06e764fec6cbb8ed0afacf24298cf2
김서영 <tjdud5387k@gmail.com>
2026-07-16 19:21:34 +0900

    5nd

---
+++ script/grade.js
@@ -0,0 +1,38 @@
+function checkGrade() {
+    var subjects = ["HTML", "CSS", "JavaScript"];
+    //과목 명이 담긴 배열 선언
+    var total = 0;
+    //점수들을 누적하여 더해줄 총 점 변수를 0으로 초기화
+
+    for(var i = 0; i < subjects.length; i++){
+        var score = Number(prompt(subjects[i] + "점수를 입력해주세요. (0~100)"));
+
+        if (isNaN(score)){ 
+            //사용자가 취소 버튼을 눌렀을 때의 예외 처리
+            alert("올바른 숫자가 입력되지 않아 계산을 취소합니다.");
+            return; //함수를 즉시 취소함
+        }
+
+        total = total + score;
+    }
+    var average = total / subjects.length;
+
+    var result = "";
+    if(average>=90){
+    result = "🎉 A등급입니다! 축하합니다";
+    } else if (average>=80){
+        result = "B등급입니다!";
+    } else if (average>=70){
+        result = "C등급입니다";
+    } else {
+        result= "D등급입니다 분발하세요"};
+
+alert (
+    "=========성적 결과 표=========\n" +
+    "총점: " + total + "점\n" +
+    "평균: " + average.toFixed(1) + "점\n" + // toFixed(1) 소수점 1번째까지만 출력해라 
+    "----------------------------\n" +
+    "결과: " + result 
+);
+}
+
---
+++ script/upDown.js
@@ -0,0 +1,42 @@
+//업다운 게임
+
+function startGame() {
+    var computerNum = Math.floor(Math.random() *50) +1;
+    var count = 0;
+    //startGame의 함수선언. / var 변수선언> 데이터를 저장하는 공간 만듦
+    // math.floor> 소수점을 버리고 정수로 만드는 내장함수/ Math.random> 0이상, 1미만의 무작위 실수를 생성하는 메서드
+    // var count 도전횟수를 기록할 초기값 설정
+
+    console.log("이번 판 컴퓨터의 비밀 숫자: " + computerNum);
+    //콘솔로그에서 숫자 확인가능 F12
+
+    while(true){
+        //무한 반복문
+        var userGuess = Number(prompt("1부터 50사이의 숫자 컴퓨터가 생각한 숫자는 무엇일까요❔❓"))
+        //prompt() 사용자에게 값을 입력받는 브라우저 창을 띄우는 함수
+        //Number() prompt로 받은 문자열 데이터를 숫자로 변환하는 형 변환 (타입 캐스팅)
+
+        if (userGuess === 0) {
+            alert("게임이 취소되었습니다.");
+            break;
+        }
+        //사용자가 취소 버튼을 누르거나 창을 닫으면 게임종료 / break 반복문 강제 종료
+
+        count = count + 1;
+
+        if (userGuess === computerNum){
+            alert("🎉정답입니다! 축하합니다! \n 👏 도전횟수 " + count + "번 만에 맞추셨습니다.");
+            break;
+        }
+        else if(userGuess > computerNum){
+            alert("⬇️ Down! 더 작은 숫자를 입력해보세요. \n (현재 " + count + "회 도전 중)");
+        }else if(userGuess < computerNum){
+            alert("⬇️ Down! 더 작은 숫자를 입력해보세요. \n (현재 " + count + "회 도전 중)");
+        }
+        else {
+            alert("⚠️입력하신 숫자가 범위내에 있지 않습니다. 다시 시도해주세요.");
+        }
+    }   
+
+}   
+
