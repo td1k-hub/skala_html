63cc72c22a87f04528af8d1f28e28e33361686be
김서영 <tjdud5387k@gmail.com>
2026-07-18 11:32:46 +0900

    13rd

---
+++ script/bag.js
@@ -0,0 +1,20 @@
+function showMyBag() {
+    var mybag = [
+        {name: "여권✈️", count:1 },
+        {name: "지갑💰", count:1 },
+        {name: "휴대폰📱", count:1 },
+        {name: "보조배터리🔋", count:2 }
+    ];
+
+    var resultText = "내 가방 속 물건들:\n------------------------\n";
+
+    for (var i = 0; i < mybag.length; i++) {
+        resultText += mybag[i].name + " : " + mybag[i].count + "개\n";
+    }
+    resultText += "------------------------\n";
+    resultText += "총 물품 종류 " + mybag.length + "개\n";
+    
+    alert(resultText);
+
+
+}
